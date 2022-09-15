639b26bfbdc78764bd43cac770e3f41b56ac1075
maocq <carmonaesc@gmail.com>
2022-09-15 17:15:29 -0500

    pool

--- java-imperative-ms/infrastructure/entry-points/api-rest/src/main/java/com/maocq/api/ApiRest.java
+++ java-imperative-ms/infrastructure/entry-points/api-rest/src/main/java/com/maocq/api/ApiRest.java
@@ -22,7 +22,32 @@ public class ApiRest {
 
     @GetMapping(path = "/hello")
     public String hello() {
-        return "Hello";
+        //return "Hello";
+        return primes(1000);
+    }
+
+    private String primes(int N)
+    {
+        boolean[] isPrime = new boolean[N + 1];
+        for (int i = 2; i <= N; i++)
+            isPrime[i] = true;
+
+        for (int i = 2; i*i <= N; i++)
+        {
+            if (isPrime[i])
+            {
+                for (int j = i; i*j <= N; j++)
+                    isPrime[i*j] = false;
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 2; i <= N; i++)
+        {
+            if (isPrime[i])
+                builder.append(i).append(" ");
+        }
+        return builder.toString();
     }
 
     @GetMapping(path = "/get-hello")
