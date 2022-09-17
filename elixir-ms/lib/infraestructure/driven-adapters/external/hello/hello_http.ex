e9a9f620a905b41443042673bbe9d132246a81e8
maocq <carmonaesc@gmail.com>
2022-09-17 18:50:05 -0500

    pool

--- elixir-ms/lib/infraestructure/driven-adapters/external/hello/hello_http.ex
+++ elixir-ms/lib/infraestructure/driven-adapters/external/hello/hello_http.ex
@@ -7,7 +7,7 @@ defmodule ElixirMs.Adapters.HelloHttp do
   def hello(latency) do
     %{ external_service_ip: external_service_ip } = SecretManagerAdapter.get_secret()
 
-    case HttpClient.request(:https, "nodes.apidevopsteam.xyz", 443, "GET", "/#{latency}") do
+    case HttpClient.request(:http, "nodes.apidevopsteam.xyz", 80, "GET", "/#{latency}") do
       {:ok, response} -> {:ok, List.first(response.data)}
       error -> error
     end
@@ -15,7 +15,7 @@ defmodule ElixirMs.Adapters.HelloHttp do
 
   def hello_connection_pool(latency) do
     %{ external_service_ip: external_service_ip } = SecretManagerAdapter.get_secret()
-    url = "https://nodes.apidevopsteam.xyz/#{latency}"
+    url = "http://nodes.apidevopsteam.xyz/#{latency}"
 
     case Finch.build(:get, url) |> Finch.request(HttpFinch, [pool_timeout: 30_000]) do
       {:ok, %Finch.Response{body: body}} -> {:ok, body}
