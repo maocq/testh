fb8dd9af0863f6b0240dc5208dd709341e610dad
maocq <carmonaesc@gmail.com>
2022-09-19 11:55:02 -0500

    pool

--- go-ms/infrastructure/driven-adapters/httpclient/hello_http_repository.go
+++ go-ms/infrastructure/driven-adapters/httpclient/hello_http_repository.go
@@ -15,14 +15,14 @@ type HelloHttpRepository struct {
 func (h *HelloHttpRepository) HelloHttp(latency string) (string, error) {
 	urlComplete := fmt.Sprintf("%s://%s/%s", "http", h.Url, latency)
 
-	return request(latency, urlComplete, &http.Client{})
+	return request(latency, urlComplete, h.Client)
 }
 
 func (h *HelloHttpRepository) HelloHttps(latency string) (string, error) {
 	//Crear nuevo cliente
 	urlComplete := fmt.Sprintf("%s://%s/%s", "https", h.Url, latency)
 
-	return request(latency, urlComplete, &http.Client{})
+	return request(latency, urlComplete, h.Client)
 }
 
 func (h *HelloHttpRepository) HelloConnectionPoolHttp1(latency string) (string, error) {
@@ -38,9 +38,6 @@ func (h *HelloHttpRepository) HelloConnectionPoolHttp2(latency string) (string, 
 }
 
 func request(latency string, url string, client *http.Client) (string, error) {
-
-	fmt.Println(url)
-
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return "", err
@@ -50,8 +47,6 @@ func request(latency string, url string, client *http.Client) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
-	fmt.Println(response.Proto)
 
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
