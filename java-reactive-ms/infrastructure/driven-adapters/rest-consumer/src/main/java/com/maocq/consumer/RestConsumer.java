127d585d060bbc60ff57ef7f6560e1f304c77e57
maocq <carmonaesc@gmail.com>
2022-09-19 21:03:12 -0500

    pool

--- java-reactive-ms/infrastructure/driven-adapters/rest-consumer/src/main/java/com/maocq/consumer/RestConsumer.java
+++ java-reactive-ms/infrastructure/driven-adapters/rest-consumer/src/main/java/com/maocq/consumer/RestConsumer.java
@@ -1,10 +1,18 @@
 package com.maocq.consumer;
 
 import com.maocq.model.hello.gateways.HelloRepository;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 @Service
 public class RestConsumer implements HelloRepository {
@@ -50,6 +58,26 @@ public class RestConsumer implements HelloRepository {
 
     @Override
     public Mono<String> helloConnectionPoolHttp2(int latency) {
+        ConnectionProvider provider =
+                ConnectionProvider.builder("custom")
+                        .maxConnections(10)
+                        .pendingAcquireMaxCount(-1)
+                        .build();
+
+        var rc = new ReactorClientHttpConnector(HttpClient.create(provider)
+                //.secure(sslContextSpec -> sslContextSpec.sslContext(sslContext))
+                .keepAlive(true)
+                .doOnConnected(connection -> {
+                    connection.addHandlerLast(new ReadTimeoutHandler(90, MILLISECONDS));
+                    connection.addHandlerLast(new WriteTimeoutHandler(90, MILLISECONDS));
+                }));
+
+        WebClient.builder()
+                .baseUrl("https://n1.apidevopsteam.xyz")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .clientConnector(rc)
+                .build();
+
         return clientPool
                 .get()
                 .uri("/{latency}", latency)
