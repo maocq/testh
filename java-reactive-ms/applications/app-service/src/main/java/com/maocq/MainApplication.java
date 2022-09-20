4af7bd8937eb9ee5b31b62849fbceba3d29368a3
maocq <carmonaesc@gmail.com>
2022-09-19 20:31:48 -0500

    pool

--- java-reactive-ms/applications/app-service/src/main/java/com/maocq/MainApplication.java
+++ java-reactive-ms/applications/app-service/src/main/java/com/maocq/MainApplication.java
@@ -2,10 +2,37 @@ package com.maocq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.netty.handler.codec.http.HttpHeaders;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.client.HttpClient;
+import reactor.util.function.Tuple2;
 
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
+		HttpClient client =
+				HttpClient.create()
+				          .protocol(HttpProtocol.H2) 
+				          .secure();                 
+
+		Tuple2<String, HttpHeaders> response =
+				client.get()
+				      .uri("https://example.com/")
+				      .responseSingle((res, bytes) -> bytes.asString()
+				                                           .zipWith(Mono.just(res.responseHeaders())))
+				      .block();
+
+        System.out.println("******************");
+        System.out.println("******************");
+        System.out.println("******************");
+        System.out.println("******************");
+        System.out.println("******************");
+        System.out.println("******************");
+        System.out.println("******************");
+		System.out.println("Used stream ID: " + response.getT2().get("x-http2-stream-id"));
+		System.out.println("Response: " + response.getT1());
+
         SpringApplication.run(MainApplication.class, args);
     }
 }
